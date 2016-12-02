e59ebd67a67f46dfe0f3e812e274f202b373e722
Marc Sánchez <sanchezfauste@gmail.com>
2016-12-02 12:04:11 +0100

    Adding proxmox api controller

---
+++ bProxmox/proxmox_api.php
@@ -0,0 +1,29 @@
+<?php
+
+require_once 'bProxmox/vendor/autoload.php';
+use ProxmoxVE\Proxmox;
+
+class ProxmoxAPI {
+
+    private static $instance = NULL;
+    private $proxmox;
+
+    private function __construct($config_file = 'bProxmox/config/proxmox_api.ini') {
+        if (file_exists($config_file)) {
+            $this->config = parse_ini_file($config_file);
+            $this->proxmox = new Proxmox($this->config);
+        } else {
+            $GLOBALS['log']->fatal("[bProxmox] Impossible to access '$config_file'.");
+        }
+    }
+
+    public static function get_instance() {
+        if (is_null(self::$instance)) {
+            self::$instance = new ProxmoxAPI();
+        }
+        return self::$instance->proxmox;
+    }
+
+}
+
+?>
