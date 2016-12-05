89cc130614e798475f604370c31c4cfee1e1e8a7
Marc Sánchez <sanchezfauste@gmail.com>
2016-12-05 12:50:20 +0100

    Adding server hdd sync to main()

--- bProxmox/main.php
+++ bProxmox/main.php
@@ -2,11 +2,14 @@
 
 require_once('bProxmox/proxmox_api.php');
 require_once('bProxmox/virtual_machine.php');
+require_once('bProxmox/server_hard_disc.php');
 
 function main() {
     $GLOBALS['log']->fatal("[bProxmox] Entering bProxmox synchronization.");
     $virtual_machine = new VirtualMachine(ProxmoxAPI::get_instance());
     $virtual_machine->sync_all_virtual_machines();
+    $server_hard_disc = new ServerHardDisc(ProxmoxAPI::get_instance());
+    $server_hard_disc->sync_all_servers_hard_discs();
     return true;
 }
 
